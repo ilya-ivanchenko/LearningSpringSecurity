4a698a9349d1eaabd0e3864e70d0c81e7ff6cc6c
ivanchenko92 <richiilya92@gmail.com>
2022-12-08 22:45:35 +0300

    Ptacticed on using custom authentication page

--- src/main/java/epam/by/ivanchenko/config/SecurityConfig.java
+++ src/main/java/epam/by/ivanchenko/config/SecurityConfig.java
@@ -5,17 +5,16 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
 
 @Configuration
 @EnableWebSecurity
-public class SecurityConfig  {        // extends
+public class SecurityConfig  {
 
     private final PersonDetailsService personDetailsService;
 
@@ -24,16 +23,54 @@ public class SecurityConfig  {        // extends
         this.personDetailsService = personDetailService;
     }
 
-    // Настройка аутентификации
-    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.userDetailsService(personDetailsService);                                       // Провайдер
-    }
 
-        @Bean
-        public PasswordEncoder getPasswordEncoder() {
-            return NoOpPasswordEncoder.getInstance();                                          // Временно! Пароль не шифруется
-            // return PasswordEncoderFactories.createDelegatingPasswordEncoder();
-        }
+    @Bean
+    public SecurityFilterChain configure(HttpSecurity http) throws Exception {
+        // конфиг. Spring Security
+        // конфиг. авторизации
+        return http
+                .csrf().disable()                                           // Откл. защиту межсайтовой подделки запросов
+                .authorizeRequests()
+                .requestMatchers("/auth/login", "/error").permitAll()
+                .anyRequest().authenticated()
+                .and()
+                .formLogin().loginPage("/auth/login")
+                .loginProcessingUrl("/process_login")
+                .defaultSuccessUrl("/hello", true)
+                .failureUrl("/auth/login?error")
+                .and().build();
+    }
+    //WebSecurityCustomizer
+
+//    @Bean
+//    public UserDetailsManager userDetailsManager(AuthenticationManagerBuilder auth) throws Exception {
+//        UserDetails person = auth.userDetailsService();
+//       // auth.userDetailsService(personDetailsService);
+//        return new
+//    }
+
+
+
+
+
+
+
+
+
+//
+//    // OLD:
+//
+//    // Настройка аутентификации
+//    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+//        auth.userDetailsService(personDetailsService);                                       // Провайдер
+//    }
+//
+//
+    @Bean
+    public PasswordEncoder getPasswordEncoder() {
+        return NoOpPasswordEncoder.getInstance();                                          // Временно! Пароль не шифруется
+        // return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+    }
 
 
 }
